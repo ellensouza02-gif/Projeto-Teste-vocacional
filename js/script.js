0c3b1c0b7225b2be2c0839b29d56296d29afdfd8
Ellen Carolyne Santos Souza <ellensouza02@professor.educacao.sp.gov.br>
2026-06-16 17:16:41 -0300

    prepara resultado para camera

--- js/script.js
+++ js/script.js
@@ -1,8 +1,156 @@
-const botao = document.getElementById("iniciar");
+let pontos = {
+
+    dev: 0,
+
+    adm: 0,
+
+    vendas: 0,
+
+    enf: 0
+
+};
 
 
-botao.addEventListener("click", () => {
+let indice = 0;
 
-    alert("Vamos iniciar o teste!");
+
+
+const perguntaTexto = document.getElementById("pergunta");
+
+const contador = document.querySelector(".progresso");
+
+const botaoSim = document.getElementById("sim");
+
+const botaoNao = document.getElementById("nao");
+
+
+
+
+// carregar pergunta
+
+function carregarPergunta(){
+
+
+    perguntaTexto.textContent =
+    perguntas[indice].pergunta;
+
+
+    contador.textContent =
+    `Pergunta ${indice + 1} de ${perguntas.length}`;
+
+
+}
+
+
+
+
+
+// botão SIM
+
+botaoSim.addEventListener("click", ()=>{
+
+
+    let resposta = perguntas[indice].sim;
+
+
+    pontos[resposta]++;
+
+
+    proximaPergunta();
+
 
 });
+
+
+
+
+
+
+// botão NÃO
+
+botaoNao.addEventListener("click", ()=>{
+
+
+    let resposta = perguntas[indice].nao;
+
+
+    pontos[resposta]++;
+
+
+    proximaPergunta();
+
+
+});
+
+
+
+
+
+
+
+function proximaPergunta(){
+
+
+    indice++;
+
+
+    if(indice < perguntas.length){
+
+
+        carregarPergunta();
+
+
+    }else{
+
+
+        mostrarResultado();
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+function mostrarResultado(){
+
+
+    let resultado = Object.keys(pontos).reduce((a,b)=>{
+
+
+        return pontos[a] > pontos[b] ? a : b;
+
+
+    });
+
+
+
+    console.log("Pontuação final:");
+
+    console.log(pontos);
+
+
+
+    console.log("Resultado:");
+
+    console.log(resultado);
+
+
+
+    alert(
+        "Seu curso ideal é: " + resultado
+    );
+
+
+}
+
+
+
+
+
+carregarPergunta();

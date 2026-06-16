7c1d4a9edb456cb5633537cc24c6c8c263379aa1
Ellen Carolyne Santos Souza <ellensouza02@professor.educacao.sp.gov.br>
2026-06-16 16:50:39 -0300

    cria navegação das perguntas

---
+++ js/perguntas.js
@@ -0,0 +1,72 @@
+const perguntas = [
+
+    {
+        pergunta: "Você gosta de criar soluções usando tecnologia?",
+        sim: "dev",
+        nao: "vendas"
+    },
+
+
+    {
+        pergunta: "Você gosta de organizar equipes e processos?",
+        sim: "adm",
+        nao: "dev"
+    },
+
+
+    {
+        pergunta: "Você gosta de conversar e convencer pessoas?",
+        sim: "vendas",
+        nao: "enf"
+    },
+
+
+    {
+        pergunta: "Você sente satisfação ajudando pessoas?",
+        sim: "enf",
+        nao: "dev"
+    },
+
+
+    {
+        pergunta: "Você gosta de computadores e programação?",
+        sim: "dev",
+        nao: "adm"
+    },
+
+
+    {
+        pergunta: "Você prefere planejar antes de executar?",
+        sim: "adm",
+        nao: "vendas"
+    },
+
+
+    {
+        pergunta: "Você gosta de trabalhar com metas e resultados?",
+        sim: "vendas",
+        nao: "enf"
+    },
+
+
+    {
+        pergunta: "Você tem interesse pela área da saúde?",
+        sim: "enf",
+        nao: "adm"
+    },
+
+
+    {
+        pergunta: "Você gosta de aprender novas tecnologias?",
+        sim: "dev",
+        nao: "vendas"
+    },
+
+
+    {
+        pergunta: "Você gosta de cuidar e orientar pessoas?",
+        sim: "enf",
+        nao: "adm"
+    }
+
+];
